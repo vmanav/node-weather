73d5083a2bf1641cc8045697b4b88494769363af
vmanav <zmanav.1999@gmail.com>
2019-10-05 00:03:02 +0530

    added a lot of cookie fucntions

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,41 @@
+
+function createCookie(key, value, maxAgeSeconds = 15) {
+
+    newCookie = `${key}=${value};max-age=${maxAgeSeconds}`;
+    console.log("The newCookie :", newCookie)
+    document.cookie = newCookie;
+    console.log("The document.cookie - ", document.cookie)
+}
+
+function readCookie(name) {
+    console.log("readCookie fn -->");
+    let key = name + "=";
+    let cookies = document.cookie.split(';');
+    console.log(cookies)
+    for (let i = 0; i < cookies.length; i++) {
+        let cookie = cookies[i];
+        // console.log(i + "th cookie is : " + cookie);
+        while (cookie.charAt(0) === ' ') {
+            cookie = cookie.substring(1, cookie.length);
+        }
+        if (cookie.indexOf(key) === 0) {
+            return cookie.substring(key.length, cookie.length);
+        }
+    }
+    return null;
+}
+
+function deleteCookie(name) {
+    // just set the cookie's expiration dat in past
+    createCookie(name, "", -1);
+}
+
+
 $(() => {
 
     let input = $('#input')
+    let submitButton = $('submitButton')
+
     input.focus(() => {
 
         let sun = $('#icon2')
@@ -14,6 +49,17 @@ $(() => {
 
     input.focusout(() => {
 
+        let cityName = input.val();
+        console.log(cityName);
+        alert(cityName);
+        createCookie("city", cityName);
+        createCookie("city1", "pehli");
+        createCookie("city2", "dusri");
+        console.log(readCookie("city"));
+        deleteCookie("city")
+        console.log(readCookie("city"));
+
+
         let sun = $('#icon2')
         sun.addClass("fa-spin")
         sun.addClass("inverted")
@@ -22,4 +68,14 @@ $(() => {
         snowflake.addClass("fa-spin")
         snowflake.addClass("inverted")
     })
+
+    submitButton.click(() => {
+        // let cityName = input.val();
+        // console.log(cityName);
+        // alert(cityName);
+        // createCookie("city", );
+    })
+
+
+
 })
