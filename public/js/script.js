134acf067d87cd5841c269d11ec945bc1a9e7c66
vmanav <zmanav.1999@gmail.com>
2019-10-08 00:03:48 +0530

    removed cookie related functions

--- public/js/script.js
+++ public/js/script.js
@@ -1,36 +1,3 @@
-
-function createCookie(key, value, maxAgeSeconds = 15) {
-
-    newCookie = `${key}=${value};max-age=${maxAgeSeconds}`;
-    console.log("The newCookie :", newCookie)
-    document.cookie = newCookie;
-    console.log("The document.cookie - ", document.cookie)
-}
-
-function readCookie(name) {
-    console.log("readCookie fn -->");
-    let key = name + "=";
-    let cookies = document.cookie.split(';');
-    console.log(cookies)
-    for (let i = 0; i < cookies.length; i++) {
-        let cookie = cookies[i];
-        // console.log(i + "th cookie is : " + cookie);
-        while (cookie.charAt(0) === ' ') {
-            cookie = cookie.substring(1, cookie.length);
-        }
-        if (cookie.indexOf(key) === 0) {
-            return cookie.substring(key.length, cookie.length);
-        }
-    }
-    return null;
-}
-
-function deleteCookie(name) {
-    // just set the cookie's expiration dat in past
-    createCookie(name, "", -1);
-}
-
-
 $(() => {
 
     let input = $('#input')
@@ -49,17 +16,6 @@ $(() => {
 
     input.focusout(() => {
 
-        let cityName = input.val();
-        console.log(cityName);
-        alert(cityName);
-        createCookie("city", cityName);
-        createCookie("city1", "pehli");
-        createCookie("city2", "dusri");
-        console.log(readCookie("city"));
-        deleteCookie("city")
-        console.log(readCookie("city"));
-
-
         let sun = $('#icon2')
         sun.addClass("fa-spin")
         sun.addClass("inverted")
@@ -68,14 +24,5 @@ $(() => {
         snowflake.addClass("fa-spin")
         snowflake.addClass("inverted")
     })
-
-    submitButton.click(() => {
-        // let cityName = input.val();
-        // console.log(cityName);
-        // alert(cityName);
-        // createCookie("city", );
-    })
-
-
 
 })
